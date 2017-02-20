775d5ec4601f9c485161dd36bee8c15ef76063a7
Ryan Lamansky <kardax@kardax.com>
2017-02-19 22:14:42 -0600

    Added a shell solution whose projects cover the overall architecture.  Included is a web UI project intended to be used with the React framework; Angular is a likely candidate to be added as an additional project in the future.

---
+++ Connexions.OpenTravel.UserInterface.React/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Connexions.OpenTravel.UserInterface.React
+{
+	static class Program
+	{
+		static void Main()
+		{
+			new WebHostBuilder()
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				.UseApplicationInsights()
+				.Build()
+				.Run()
+				;
+		}
+	}
+}
