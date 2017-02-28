7bbabf9c2547817dd80792673c7447d88037b281
Ryan Lamansky <kardax@kardax.com>
2017-02-27 22:46:01 -0600

    Added SASS/SCSS process to Gulp, minor gulpfile.js cleanup.

--- Connexions.OpenTravel.UserInterface.React/gulpfile.js
+++ Connexions.OpenTravel.UserInterface.React/gulpfile.js
@@ -1,20 +1,32 @@
-﻿/// <binding AfterBuild='webpack' Clean='clean-temporary' />
+﻿/// <binding AfterBuild='_build' Clean='clean-temporary' />
 "use strict";
 const gulp = require("gulp");
 const clean = require("gulp-clean");
 const webpack = require("webpack-stream");
-
-//The webpack implementation here is sub-optimal: it's not leveraging gulp's streaming capability, instead basically just running webpack.
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
 
 // Clears the /build folder which is used to stage TypeScript output for webpack bundling.
 gulp.task("clean-temporary", () => gulp
-	.src("./build/*", { read: false }) //Don't actually need to read these files sicne we're deleting them.
+	.src("./build/*", { read: false }) //Don't actually need to read these files since we're deleting them.
 	.pipe(clean())
 );
 
-//
+//Merges all script files into a single bundle.
 gulp.task("webpack", ["clean-temporary"], () => gulp
 	.src("./App", { read: false }) //Read by webpack, only a placeholder here.
 	.pipe(webpack(require("./webpack.config.js"), require('webpack')))
 	.pipe(gulp.dest("./wwwroot/build/"))
 );
+
+// Runs the SASS compiler to convert SCSS to standard CSS.
+gulp.task("sass", () => gulp
+	.src(["./**/*.scss", "!./node_modules/**/*", "!./wwwroot/**/*"])
+	.pipe(sourcemaps.init())
+	.pipe(sass().on("error", sass.logError))
+	.pipe(sourcemaps.write("."))
+	.pipe(gulp.dest("./wwwroot/build/"))
+);
+
+// Does nothing itself, runs all dependencies in parallel, intended to be run after the .NET code build is successful.
+gulp.task("_build", ["webpack", "sass"]);
