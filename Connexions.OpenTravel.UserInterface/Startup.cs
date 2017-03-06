8acac58df5fc54992d05bf24601da65c1a90629a
Ryan Lamansky <kardax@kardax.com>
2017-03-05 20:13:14 -0600

    Renamed projects in preparation for namespace depth reduction.

--- Connexions.OpenTravel.UserInterface/Startup.cs
+++ Connexions.OpenTravel.UserInterface/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Connexions.OpenTravel.UserInterface.Tests")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Connexions.Travel.Tests")]
 
 namespace Connexions.OpenTravel.UserInterface
 {
