2e2bb6ed57723182adab029ade1f0971439f4682
Ryan Lamansky <kardax@kardax.com>
2017-04-08 11:30:25 -0500

    Added automatic HTTPS redirect for sites hosted at *.azurewebsites.net, which has a free wildcard SSL certificate.

--- Connexions.Travel/Startup.cs
+++ Connexions.Travel/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.IO.Compression;
+using System.Threading.Tasks;
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Connexions.Travel.Tests")]
 
@@ -41,11 +42,36 @@ namespace Connexions.Travel
 
 			app
 				.UseResponseCompression()
+				.MapWhen(HttpsRedirectCondition, a => a.Run(HttpsRedirectPermanent))
 				.UseDefaultFiles()
 				.UseStaticFiles()
 				.Map("/Session", a => a.UseWebSockets().Use(Session.WebSocketHandlerAsync))
 				.Map("/ImageProcessor/Resize", ImageProcessor.Resize.Configure)
 				;
+		}
+
+		/// <summary>
+		/// Determines whether the request should be redirected to HTTPS.
+		/// </summary>
+		/// <param name="context">Context for the request.</param>
+		/// <returns>True if it should be redirected to HTTPS, otherwise false.</returns>
+		private static bool HttpsRedirectCondition(Microsoft.AspNetCore.Http.HttpContext context)
+			=> context.Request.IsHttps == false
+			&& context.Request.Host.HasValue
+			&& context.Request.Host.Port.HasValue == false //If a non-default port is used, the HTTPS port is unknown.
+			&& context.Request.Host.Host.EndsWith(".azurewebsites.net")
+			;
+
+		/// <summary>
+		/// Indicates that the desired resource has permanently moved to an HTTPS-based URL with with same host.
+		/// </summary>
+		/// <param name="context">Context for the request.</param>
+		/// <returns><see cref="Task.CompletedTask"/>.</returns>
+		private static Task HttpsRedirectPermanent(Microsoft.AspNetCore.Http.HttpContext context)
+		{
+			var path = "https://" + context.Request.Host.Value + context.Request.Path;
+			context.Response.Redirect(path, true);
+			return Task.CompletedTask;
 		}
 	}
 }
