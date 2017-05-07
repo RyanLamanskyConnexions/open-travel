b5d5bf0b7471e0355df267f697e75d3a17f96e6e
Ryan Lamansky <kardax@kardax.com>
2017-05-07 16:11:58 -0500

    Integrated webpack into the publish process.  Removed webpack from Gulp due to it being run automatically by ASP.NET during startup.  Set up dev/prod web.config variants.

--- Connexions.Travel.React/gulpfile.js
+++ Connexions.Travel.React/gulpfile.js
@@ -1,23 +1,8 @@
-﻿/// <binding AfterBuild='_build' Clean='clean-temporary' />
+﻿/// <binding AfterBuild='sass' />
 "use strict";
 const gulp = require("gulp");
-const clean = require("gulp-clean");
-const webpack = require("webpack-stream");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
-
-// Clears the /build folder which is used to stage TypeScript output for webpack bundling.
-gulp.task("clean-temporary", () => gulp
-	.src("./build/*", { read: false }) //Don't actually need to read these files since we're deleting them.
-	.pipe(clean())
-);
-
-//Merges all script files into a single bundle.
-gulp.task("webpack", ["clean-temporary"], () => gulp
-	.src("./App", { read: false }) //Read by webpack, only a placeholder here.
-	.pipe(webpack(require("./webpack.config.js"), require('webpack')))
-	.pipe(gulp.dest("./wwwroot/build/"))
-);
 
 // Runs the SASS compiler to convert SCSS to standard CSS.
 gulp.task("sass", () => gulp
@@ -27,6 +12,3 @@ gulp.task("sass", () => gulp
 	.pipe(sourcemaps.write("."))
 	.pipe(gulp.dest("./wwwroot/build/"))
 );
-
-// Does nothing itself, runs all dependencies in parallel, intended to be run after the .NET code build is successful.
-gulp.task("_build", ["webpack", "sass"]);
--- Connexions.Travel.React/webpack.config.js
+++ Connexions.Travel.React/webpack.config.js
@@ -1,48 +1,58 @@
-﻿const webpack = require("webpack");
-const path = require("path");
+﻿"use strict";
 
-module.exports = {
-	entry: {
-		main: ["./Main"]
-	},
-	output: {
-		path: path.resolve(__dirname + "/wwwroot/", "build"),
-		publicPath: "/build/",
-		filename: "bundle.js"
-	},
-	resolve: {
-		extensions: [".ts", ".tsx", ".js"]
-	},
-	module: {
-		rules: [
-			{
-				//ASP.NET Core WebpackDevMiddlewareOptions.HotModuleReplacement requires babel.
-				//TypeScript alone would otherwise be enough.
-				test: /\.ts(x?)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						cacheDirectory: true
+const webpack = require("webpack");
+const path = require("path");
+const bundleOutputDir = './wwwroot/build';
+
+module.exports = env => {
+	const isDevBuild = !(env && env.prod);
+	return {
+		entry: {
+			main: ["./Main"]
+		},
+		output: {
+			path: path.join(__dirname, bundleOutputDir),
+			publicPath: "/build/",
+			filename: "bundle.js"
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		},
+		module: {
+			rules: [
+				{
+					//ASP.NET Core WebpackDevMiddlewareOptions.HotModuleReplacement requires babel.
+					//TypeScript alone would otherwise be enough.
+					test: /\.ts(x?)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							cacheDirectory: true
+						}
 					}
-				}
-			},
-			{
-				test: /\.tsx?$/,
-				exclude: /node_modules/,
-				use: 'ts-loader'
-			},
-		]
-	},
-	devtool: "source-map",
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production')
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true
-		})
-	]
+				},
+				{
+					test: /\.tsx?$/,
+					exclude: /node_modules/,
+					use: 'ts-loader'
+				},
+			]
+		},
+		plugins: isDevBuild ? [
+			// Plugins that apply in development builds only
+			new webpack.SourceMapDevToolPlugin({
+				filename: "[file].map",
+				moduleFilenameTemplate: path.relative(bundleOutputDir, "[resourcePath]")
+			})
+		] : [
+				// Plugins that apply in production builds only
+				new webpack.DefinePlugin({
+					'process.env': {
+						NODE_ENV: JSON.stringify('production')
+					}
+				}),
+				new webpack.optimize.UglifyJsPlugin()
+			]
+	}
 };
