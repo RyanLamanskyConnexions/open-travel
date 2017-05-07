81dcf30b6aa75bb8ed5a2ac75a12bc7d4f301329
Ryan Lamansky <kardax@kardax.com>
2017-05-07 17:34:16 -0500

    The Gulp process, which now only contains the SASS utility, runs with publish actions and upon project open.  It has to be manually triggered for updates otherwise.  This avoids wasted build time when working on the front end or back end separately.

--- Connexions.Travel.React/gulpfile.js
+++ Connexions.Travel.React/gulpfile.js
@@ -1,11 +1,11 @@
-﻿/// <binding AfterBuild='sass' />
+﻿/// <binding ProjectOpened='default' />
 "use strict";
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 
 // Runs the SASS compiler to convert SCSS to standard CSS.
-gulp.task("sass", () => gulp
+gulp.task("default", () => gulp
 	.src(["./**/*.scss", "!./node_modules/**/*", "!./wwwroot/**/*"])
 	.pipe(sourcemaps.init())
 	.pipe(sass().on("error", sass.logError))
