90c78f3fd8a33343001928f0afaccce09eb17ac7
Ryan Lamansky <rlamansky@cxloyalty.com>
2017-05-08 13:33:14 -0500

    Fixed IE11 compatibility via Babel polyfill (this worked fine pre-Babel when TypeScript used its built-in polyfill feature).

--- Connexions.Travel.React/Main.tsx
+++ Connexions.Travel.React/Main.tsx
@@ -1,5 +1,6 @@
 ﻿import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Session from "./Session";
+import "babel-polyfill"; //Needed for IE11 compatibility when Babel is used (TypeScript alone doesn't require a polyfill).
 
 ReactDOM.render(<Session />, document.getElementById("root"));
